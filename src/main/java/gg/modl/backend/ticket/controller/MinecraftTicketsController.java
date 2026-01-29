5ce9ab03f985361126bdab08aa5ce72487351c73
Theo <t@bongbong.com>
2026-01-28 16:59:25 -0800

    Update MinecraftTicketsController.java

--- src/main/java/gg/modl/backend/ticket/controller/MinecraftTicketsController.java
+++ src/main/java/gg/modl/backend/ticket/controller/MinecraftTicketsController.java
@@ -17,6 +17,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
