f9ca3807de3dd85d2586228154c1c10819f81874
byteful <info@byteful.me>
2026-01-29 22:15:57 -0500

    Fix punishment types

--- src/main/java/gg/modl/backend/config/RestTemplateConfig.java
+++ src/main/java/gg/modl/backend/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package gg.modl.backend.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -20,6 +21,7 @@ public class RestTemplateConfig {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
         return mapper;
     }
 }
