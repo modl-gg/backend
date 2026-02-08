31156e1cbd1c80bdb09557ef1312c92e258a7dee
Theo <t@bongbong.com>
2026-02-07 20:53:34 -0800

    Update ServerProvisioningService.java

--- src/main/java/gg/modl/backend/server/service/ServerProvisioningService.java
+++ src/main/java/gg/modl/backend/server/service/ServerProvisioningService.java
@@ -363,7 +363,7 @@ public class ServerProvisioningService {
                         .isEnabled(true)
                         .ordinal(1)
                         .createdAt(now)
-                        .updat
+                        .updatedAt(now)
                         .build(),
                 HomepageCard.builder()
                         .title("Contact Us")
