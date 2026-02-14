b50eb980efa901ca996d77e1a78c1b9d79895f1a
Theo <t@bongbong.com>
2026-02-13 18:27:13 -0800

    Update FilterConfig.java

--- src/main/java/gg/modl/backend/rest/middleware/FilterConfig.java
+++ src/main/java/gg/modl/backend/rest/middleware/FilterConfig.java
@@ -1,6 +1,5 @@
 package gg.modl.backend.rest.middleware;
 
-import gg.modl.backend.cors.DynamicCorsConfigurationSource;
 import gg.modl.backend.rest.RESTMappingV1;
 import gg.modl.backend.server.ServerService;
 import lombok.RequiredArgsConstructor;
@@ -9,14 +8,11 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class FilterConfig {
     private final ServerService serverService;
-    private final DynamicCorsConfigurationSource corsConfigurationSource;
     private final ApiKeyFilter apiKeyFilter;
 
     @Value("${modl.development-mode:false}")
@@ -24,15 +20,6 @@ public class FilterConfig {
 
     @Value("${modl.dev.server-domain:}")
     private String devServerDomain;
-
-    @Bean
-    public FilterRegistrationBean<CorsFilter> corsFilter() {
-        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new CorsFilter((CorsConfigurationSource) corsConfigurationSource));
-        registrationBean.addUrlPatterns("/*");
-        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return registrationBean;
-    }
 
     @Bean
     public FilterRegistrationBean<ServerHeaderFilter> serverDomainFilter() {
