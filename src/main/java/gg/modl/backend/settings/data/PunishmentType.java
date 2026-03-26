7e509d7f67a515c4de2c34e2e133d4590b482501
byteful <info@byteful.me>
2026-03-26 11:58:07 -0400

    Fix punishment types

--- src/main/java/gg/modl/backend/settings/data/PunishmentType.java
+++ src/main/java/gg/modl/backend/settings/data/PunishmentType.java
@@ -1,6 +1,5 @@
 package gg.modl.backend.settings.data;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class PunishmentType {
-    @JsonAlias("_id")
     private Integer id;
     private String name;
     private String category;
